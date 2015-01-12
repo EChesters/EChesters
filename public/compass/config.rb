2205fca1b4ef16eb4e794bca07322dd7f5992de2
Liloka <elizabeth.chesters@gmail.com>
2015-01-12 15:35:47 +0000

    Updated index to include css files

---
+++ public/compass/config.rb
@@ -0,0 +1,11 @@
+require 'griddle'
+
+relative_assets  = true
+css_dir          = "../css"
+sass_dir         = "sass"
+
+output_style     = :expanded
+
+line_comments    = false
+
+preferred_syntax = :scss
